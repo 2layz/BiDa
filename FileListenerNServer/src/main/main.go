c6612f20ad894b13ffb2222efc6344327cf9c07f
2layz <vivek83ms@gmail.com>
2018-01-21 00:22:32 -0800

    changes for publishing custom metrics to cadvisor

--- FileListenerNServer/src/main/main.go
+++ FileListenerNServer/src/main/main.go
@@ -7,16 +7,37 @@ import (
 	"os"
 )
 
+func handleMetrics(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("----------------  Servicing metrics request  --------------")
+    msg := 10
+    fmt.Fprintf(w, "# TYPE msg gauge\nmsg %d\n" , msg )
+}
+
 func main() {
 	datadir := os.Getenv(filewatcher.DATA_DIR_ENV)
-	fs := http.FileServer(http.Dir(datadir))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	role := os.Getenv("ROLE")
+        fmt.Println("ROLE   --------------> "+role)
+
+        if role == "fileserver"{
+   	   fs := http.FileServer(http.Dir(datadir))
+	   http.Handle("/static/", http.StripPrefix("/static/", fs))
+	   http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello! try /static/<filename>\n")
-	})
+	   })
 
-	go filewatcher.StartWatching()
+        }
 
+
+	if role == "publisher" {
+           fmt.Println("Starting PUBLISHER...")
+	   http.HandleFunc("/metrics/", func(w http.ResponseWriter, r *http.Request) {
+    		fmt.Println("----------------  Servicing metrics request  --------------")
+    		msg := 10
+    		fmt.Fprintf(w, "# TYPE msg gauge\nmsg %d\n" , msg )
+
+	   })
+	   go filewatcher.StartWatching()
+	}
 	fmt.Println("Listening...")
 	http.ListenAndServe(":8080", nil)
 
